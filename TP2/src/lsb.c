8223c72b623f74bc1b12c19e78ef0d985b8a8138
Juan Pablo Civile <elementohb@gmail.com>
2013-06-02 14:14:06 -0300

    Fix how bits are handled in LSB* algorithms

--- TP2/src/lsb.c
+++ TP2/src/lsb.c
@@ -9,7 +9,7 @@ void lsb_embed(struct data* img, struct data* data, size_t n) {
     unsigned char mask = (1 << n) - 1;
     for (size_t byte = 0; byte < data->len; byte++) {
 
-        for (size_t bitOnByte = 0; bitOnByte < 8; bitOnByte += n, byteOnImage++) {
+        for (int bitOnByte = 8 - n; bitOnByte >= 0; bitOnByte -= n, byteOnImage++) {
             img->bytes[byteOnImage] ^= img->bytes[byteOnImage] & mask;
             img->bytes[byteOnImage] |= (data->bytes[byte] >> bitOnByte) & mask;
         }
@@ -24,17 +24,17 @@ void lsb_extract(struct data* source, struct data* out, size_t n) {
     memset(buffer, 0, maxOutputSize);
 
     size_t byte = 0;
-    size_t bitOnByte = 0;
+    int bitOnByte = 8 - n;
 
     unsigned char mask = (1 << n) - 1;
     for (size_t byteOnImage = BMP_HEADER_SIZE; byteOnImage < source->len; byteOnImage++) {
 
         buffer[byte] |= (source->bytes[byteOnImage] & mask) << bitOnByte;
 
-        bitOnByte += n;
-        if (bitOnByte == 8) {
+        bitOnByte -= n;
+        if (bitOnByte < 0) {
             byte++;
-            bitOnByte = 0;
+            bitOnByte = 8 - n;
         }
 
     }
